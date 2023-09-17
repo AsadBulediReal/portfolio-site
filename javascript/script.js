3b7645c300ca6fb5310602765fbd061848899129
Asad Buledi <bulediasadjamil@gmail.com>
2023-09-17 16:09:44 +0500

    site is responsive now

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,6 @@
 const allSections = document.querySelectorAll("section");
+const hamburgerMenue = document.querySelector(".hamburger-menu");
+const navList = document.querySelector(".nav-container");
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -6,9 +8,20 @@ const observer = new IntersectionObserver(
       section.target.classList.toggle("show", section.isIntersecting);
     });
   },
-  { threshold: 0.4 }
+  { threshold: 0 }
 );
 
 allSections.forEach((section) => {
   observer.observe(section);
+});
+
+const onClick = () => {
+  hamburgerMenue.classList.toggle("active");
+  navList.classList.toggle("active");
+};
+
+hamburgerMenue.addEventListener("click", onClick);
+
+document.querySelectorAll(".nav-item").forEach((item) => {
+  item.addEventListener("click", onClick);
 });
