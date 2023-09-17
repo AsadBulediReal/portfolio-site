689ee5fcd4a224356ec311694bd2d02e19806c1a
Asad Buledi <bulediasadjamil@gmail.com>
2023-09-17 20:59:31 +0500

    added bubble effect

--- javascript/script.js
+++ javascript/script.js
@@ -25,3 +25,125 @@ hamburgerMenue.addEventListener("click", onClick);
 document.querySelectorAll(".nav-item").forEach((item) => {
   item.addEventListener("click", onClick);
 });
+
+// Get the container element
+const container = document.getElementById("bubble-container");
+
+// Set the container dimensions to match the viewport size
+
+// Create an array to store the bubbles
+const bubbles = [];
+
+// Create a function to generate a random number between min and max
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// Create a Bubble class
+class Bubble {
+  constructor(x, y, radius, speed) {
+    this.element = document.createElement("div");
+    this.element.className = "bubble";
+    this.element.style.width = radius * 2 + "px";
+    this.element.style.height = radius * 2 + "px";
+    this.element.style.left = x - radius + "px";
+    this.element.style.top = y - radius + "px";
+    this.radius = radius;
+    this.speed = speed;
+    container.appendChild(this.element);
+
+    this.element.addEventListener("click", () => {
+      this.pop();
+    });
+  }
+
+  update() {
+    this.element.style.top =
+      parseInt(this.element.style.top) - this.speed + "px";
+  }
+
+  pop() {
+    const popText = document.createElement("div");
+    popText.textContent = "Popped";
+    popText.classList.add("pop-text");
+
+    // Position the text outside the bubble
+    popText.style.position = "absolute";
+    popText.style.left = parseInt(this.element.style.left) + "px";
+    popText.style.top = parseInt(this.element.style.top) - 20 + "px";
+
+    container.appendChild(popText);
+
+    this.element.classList.add("pop");
+    setTimeout(() => {
+      if (container.contains(this.element)) {
+        container.removeChild(this.element);
+        bubbles.splice(bubbles.indexOf(this), 1);
+      }
+      if (container.contains(popText)) {
+        setTimeout(() => {
+          if (container.contains(popText)) {
+            container.removeChild(popText);
+          }
+        }, 500); // Remove the text after 0.5 seconds
+      }
+    }, 500);
+
+    setTimeout(() => {
+      if (container.contains(popText)) {
+        container.removeChild(popText);
+      }
+    }, 2000);
+  }
+}
+
+// Create a function to animate the bubbles
+function animate() {
+  bubbles.forEach((bubble) => {
+    bubble.update();
+
+    if (parseInt(bubble.element.style.top) + bubble.radius * 2 < 0) {
+      bubble.pop();
+    }
+  });
+
+  requestAnimationFrame(animate);
+}
+
+// Create a function to handle mouse clicks
+function handleClick(event) {
+  const mouseX = event.clientX;
+
+  const mouseY = event.clientY;
+  bubbles.forEach((bubble) => {
+    const rect = bubble.element.getBoundingClientRect();
+    const bubbleX = rect.left + bubble.radius;
+    const bubbleY = rect.top + bubble.radius;
+    const dx = bubbleX - mouseX;
+    const dy = bubbleY - mouseY;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < bubble.radius) {
+      bubble.pop();
+    }
+  });
+}
+
+// Attach the click event listener to the document
+document.addEventListener("click", handleClick);
+
+// Start the animation
+animate();
+
+// Create bubbles every 1 second if there are less than 10 bubbles
+
+setInterval(() => {
+  if (bubbles.length < 20) {
+    const x = getRandomNumber(0, container.offsetWidth);
+    const y = document.body.scrollHeight + 140;
+    const radius = getRandomNumber(35, 55); // Increase the range for bigger bubbles
+    const speed = getRandomNumber(1, 3);
+    const bubble = new Bubble(x, y, radius, speed);
+    bubbles.push(bubble);
+  }
+}, 1000);
